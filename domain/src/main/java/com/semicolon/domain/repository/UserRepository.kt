862b15fe94aa952d613ae6121251c3c99bfcdd0f
shw0471 <shw0471@naver.com>
2022-04-01 10:47:05 +0900

    ⚡️ :: update UserRepository interface

---
+++ domain/src/main/java/com/semicolon/domain/repository/UserRepository.kt
@@ -0,0 +1,21 @@
+package com.semicolon.domain.repository
+
+import com.semicolon.domain.entity.user.ProfileEntity
+import com.semicolon.domain.param.user.LoginParam
+import com.semicolon.domain.param.user.ProfileParam
+import com.semicolon.domain.param.user.SignUpParam
+
+interface UserRepository {
+
+    suspend fun autoLogin()
+
+    suspend fun login(loginParam: LoginParam)
+
+    suspend fun signUp(signUpParam: SignUpParam)
+
+    suspend fun removeAccount()
+
+    suspend fun editProfile(profileParam: ProfileParam)
+
+    suspend fun fetchProfile(): ProfileEntity
+}
