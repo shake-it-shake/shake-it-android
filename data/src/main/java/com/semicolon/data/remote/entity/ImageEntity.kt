9c38f54ff38998582045b40395b53ff96d93851f
shw0471 <shw0471@naver.com>
2022-04-09 06:10:59 +0900

    📑 :: add entity classes

---
+++ data/src/main/java/com/semicolon/data/remote/entity/ImageEntity.kt
@@ -0,0 +1,6 @@
+package com.semicolon.data.remote.entity
+
+import java.io.File
+
+@JvmInline
+value class ImageEntity(val image: File)
---
+++ data/src/main/java/com/semicolon/data/remote/entity/ImageUrlEntity.kt
@@ -0,0 +1,4 @@
+package com.semicolon.data.remote.entity
+
+@JvmInline
+value class ImageUrlEntity(val imageUrl: String)
