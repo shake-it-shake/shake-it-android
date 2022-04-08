ae6f4c2c8b927c0b4b553ae56ca28f34290f787d
shw0471 <shw0471@naver.com>
2022-04-09 06:18:34 +0900

    ⚡️ :: add extend funcs

--- data/src/main/java/com/semicolon/data/remote/response/image/ImageResponse.kt
+++ data/src/main/java/com/semicolon/data/remote/response/image/ImageResponse.kt
@@ -1,7 +1,10 @@
 package com.semicolon.data.remote.response.image
 
 import com.google.gson.annotations.SerializedName
+import com.semicolon.data.remote.entity.ImageUrlEntity
 
 data class ImageResponse(
     @SerializedName("link") val link: String
 )
+
+fun ImageResponse.toEntity() = ImageUrlEntity(link)
