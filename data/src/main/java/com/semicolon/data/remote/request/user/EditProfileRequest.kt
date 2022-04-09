0d8a422a10c2a5e974b89e70f31a5bd93d7af34a
shw0471 <shw0471@naver.com>
2022-04-09 21:17:11 +0900

    ⚡️ :: update mapping extend functions

--- data/src/main/java/com/semicolon/data/remote/request/user/EditProfileRequest.kt
+++ data/src/main/java/com/semicolon/data/remote/request/user/EditProfileRequest.kt
@@ -1,8 +1,15 @@
 package com.semicolon.data.remote.request.user
 
 import com.google.gson.annotations.SerializedName
+import com.semicolon.domain.entity.user.EditProfileEntity
 
 data class EditProfileRequest(
     @SerializedName("nickname") val nickname: String,
     @SerializedName("image_path") val imagePath: String
 )
+
+fun EditProfileEntity.toRequest(imagePath: String) =
+    EditProfileRequest(
+        nickname = nickname,
+        imagePath = imagePath
+    )
