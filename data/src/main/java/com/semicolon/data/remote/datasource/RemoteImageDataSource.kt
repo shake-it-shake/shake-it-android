9a3b80796963f08b6a605718c980f8ee61fc5343
shw0471 <shw0471@naver.com>
2022-04-09 21:19:33 +0900

    ⚰️ :: remote image data source

--- data/src/main/java/com/semicolon/data/remote/datasource/RemoteImageDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.semicolon.data.remote.datasource
-
-import com.semicolon.data.remote.entity.ImageEntity
-import com.semicolon.data.remote.entity.ImageUrlEntity
-
-interface RemoteImageDataSource {
-
-    suspend fun sendImage(imageEntity: ImageEntity): ImageUrlEntity
-}
