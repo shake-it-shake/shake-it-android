119e890b0dc8361a2c844a9bfb86b67ab21ba7d5
shw0471 <shw0471@naver.com>
2022-04-13 17:18:57 +0900

    ⚡️ :: update base view model

---
+++ ui/src/main/java/com/semicolon/shakeit/base/BaseViewModel.kt
@@ -0,0 +1,25 @@
+package com.semicolon.shakeit.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel<T> : ViewModel() {
+
+    private val _eventFlow = MutableEventFlow<T>()
+    val eventFlow = _eventFlow.asEventFlow()
+
+    protected suspend fun emitEvent(event: T) {
+        _eventFlow.emit(event)
+    }
+
+    protected fun <V> execute(
+        job: suspend () -> V,
+        onSuccess: suspend (value: V) -> Unit,
+        onFailure: suspend (t: Throwable) -> Unit
+    ) = viewModelScope.launch {
+        kotlin.runCatching { job() }
+            .onSuccess { onSuccess(it) }
+            .onFailure { onFailure(it) }
+    }
+}
