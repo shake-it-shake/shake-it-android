d155b2d60e63c873e258ae54a93cd1730e46b031
shw0471 <shw0471@naver.com>
2022-04-13 14:35:45 +0900

    ⚙️ :: set up navigation

--- ui/src/main/java/com/semicolon/shakeit/MainActivity.kt
+++ ui/src/main/java/com/semicolon/shakeit/MainActivity.kt
@@ -3,41 +3,126 @@ package com.semicolon.shakeit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.semicolon.shakeit.ui.theme.ShakeitTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ShakeitTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContent { ShakeitTheme { ShakeIt() } }
+    }
+}
+
+@Composable
+fun ShakeIt() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Splash.route
+    ) {
+        composable(Screen.Splash.route) {
+            // TODO 스플래쉬 화면으로 이동
+        }
+        composable(Screen.Login.route) {
+            // TODO 로그인 화면으로 이동
+        }
+        composable(Screen.SignUp.route) {
+            // TODO 회원가입 화면으로 이동
+        }
+        composable(Screen.Main.route) {
+            MainScaffold()
+        }
+        composable(Screen.Club.route) {
+            // TODO 클럽 화면으로 이동
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScaffold() {
+    val scaffoldState = rememberScaffoldState()
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            BottomNavigation {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+                val items = listOf(
+                    BottomNavigationScreen.Club,
+                    BottomNavigationScreen.Friend,
+                    BottomNavigationScreen.Profile
+                )
+                items.forEach { screen ->
+                    BottomNavigationItem(
+                        icon = screen.icon,
+                        label = { Text(screen.label) },
+                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                        onClick = {
+                            navController.navigate(screen.route)
+                        }
+                    )
+                }
+            }
+        },
+        scaffoldState = scaffoldState
+    ) {
+        NavHost(
+            navController = navController,
+            startDestination = BottomNavigationScreen.Club.route
+        ) {
+            composable(BottomNavigationScreen.Club.route) {
+                // TODO 클럽 목록 화면으로 이동
+            }
+            composable(BottomNavigationScreen.Friend.route) {
+                // TODO 친구 화면으로 이동
+            }
+            composable(BottomNavigationScreen.Profile.route) {
+                // TODO 프로필 화면으로 이동
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ShakeitTheme {
-        Greeting("Android")
-    }
+sealed class Screen(val route: String) {
+    object Splash : Screen("splash")
+
+    object Login : Screen("login")
+
+    object SignUp : Screen("signup")
+
+    object Main : Screen("main")
+
+    object Club : Screen("club")
+}
+
+sealed class BottomNavigationScreen(
+    val route: String,
+    val icon: @Composable () -> Unit,
+    val label: String
+) {
+    object Club : BottomNavigationScreen(
+        route = "main/club",
+        icon = { Icon(painterResource(R.drawable.ic_disco), null) },
+        label = "클럽"
+    )
+
+    object Friend : BottomNavigationScreen(
+        route = "main/friend",
+        icon = { Icon(painterResource(R.drawable.ic_friend), null) },
+        label = "친구"
+    )
+
+    object Profile : BottomNavigationScreen(
+        route = "main/profile",
+        icon = { Icon(painterResource(R.drawable.ic_profile), null) },
+        label = "프로필"
+    )
 }
