4e924c2e848bd93279d854819d7425dc5252857b
shw0471 <shw0471@naver.com>
2022-04-13 19:54:51 +0900

    📑 :: add fetchFriendRequests() function

--- data/src/main/java/com/semicolon/data/remote/api/FriendApi.kt
+++ data/src/main/java/com/semicolon/data/remote/api/FriendApi.kt
@@ -3,6 +3,7 @@ package com.semicolon.data.remote.api
 import com.semicolon.data.remote.request.friend.AcceptFriendRequest
 import com.semicolon.data.remote.request.friend.AddFriendRequest
 import com.semicolon.data.remote.request.friend.DeleteFriendRequest
+import com.semicolon.data.remote.response.friend.FriendRequestsResponse
 import com.semicolon.data.remote.response.friend.FriendsResponse
 import retrofit2.http.*
 
@@ -25,4 +26,7 @@ interface FriendApi {
     suspend fun deleteFriend(
         @Body deleteFriendRequest: DeleteFriendRequest
     )
+
+    @GET("/friends/requests")
+    suspend fun fetchFriendRequests(): FriendRequestsResponse
 }
