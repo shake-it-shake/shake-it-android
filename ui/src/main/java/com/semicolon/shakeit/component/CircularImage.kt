5bd2716be3b8d3330707a598280aadf66e362293
shw0471 <shw0471@naver.com>
2022-04-14 16:28:06 +0900

    ⚡️ :: update circular image

---
+++ ui/src/main/java/com/semicolon/shakeit/component/CircularImage.kt
@@ -0,0 +1,38 @@
+package com.semicolon.shakeit.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import java.io.File
+
+@Composable
+fun CircularImage(image: File?, size: Dp, modifier: Modifier = Modifier) {
+    Image(
+        painter = rememberImagePainter(image),
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = modifier
+            .size(size)
+            .clip(CircleShape)
+    )
+}
+
+@Composable
+fun CircularImage(imageUrl: String, radius: Dp, modifier: Modifier = Modifier) {
+    Image(
+        painter = rememberImagePainter(imageUrl),
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = modifier
+            .size(radius)
+            .clip(CircleShape)
+    )
+}
