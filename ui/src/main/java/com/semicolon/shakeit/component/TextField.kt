43ec56f3a139a21a3d771c12843ecd140acd553e
shw0471 <shw0471@naver.com>
2022-04-14 10:14:16 +0900

    ⚰️ :: delete useless imports

--- ui/src/main/java/com/semicolon/shakeit/component/TextField.kt
+++ ui/src/main/java/com/semicolon/shakeit/component/TextField.kt
@@ -1,6 +1,5 @@
 package com.semicolon.shakeit.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,7 +7,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.LocalTextStyle
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
